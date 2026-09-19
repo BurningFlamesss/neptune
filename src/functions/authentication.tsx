bc0b451ea9dc5525d44a159bebf9cbd99372b701
AD <burningggflamesss@gmail.com>
2026-09-19 11:55:28 +0545

    add a server function to register thirdparty app via providing clientId and clientSecret

---
+++ src/functions/authentication.tsx
@@ -0,0 +1,27 @@
+import { createServerFn } from "@tanstack/react-start";
+import { z } from "zod";
+import { auth } from "#/lib/auth.ts";
+
+const registerThirdPartyAppSchema = z.object({
+	appName: z.string(),
+	redirectUri: z.url(),
+});
+
+export const registerThirdPartyApp = createServerFn()
+	.validator(registerThirdPartyAppSchema)
+	.handler(async ({ data, context }) => {
+		const client = await auth.api.createOAuthClient({
+			body: {
+				client_name: data.appName,
+				redirect_uris: [data.redirectUri],
+				grant_types: ["authentication_code", "refresh_token"],
+				response_types: ["code"],
+				token_endpoint_auth_method: "client_secret_basic",
+			},
+		});
+
+		return {
+			clientId: client.client_id,
+			clientSecret: client.client_secret,
+		};
+	});
