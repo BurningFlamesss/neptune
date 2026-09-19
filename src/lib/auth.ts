75fc7991b84aac138ca32817b2c9755120d41093
AD <burningggflamesss@gmail.com>
2026-09-19 09:25:21 +0545

    wrote the schema for the authentication in the application

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import { serverEnv } from "#/env/serverEnv.ts";
 
 const { prisma } = await import("../db");
 
@@ -10,6 +11,40 @@ export const auth = betterAuth({
 	}),
 	emailAndPassword: {
 		enabled: true,
+		requireEmailVerification: true,
+		revokeSessionsOnPasswordReset: true,
+
+		async sendResetPassword({ user, url, token }, request) {
+			console.table({ user, url, token });
+
+			// TODO: Send verification Email
+		},
+
+		resetPasswordTokenExpiresIn: 15 * 60 * 1000, // 15 minutes
+
+		async onExistingUserSignUp({ user }, request) {
+			if (user.emailVerified) {
+				console.table({
+					to: user.email,
+					subject: "Sign up attempt with your email",
+				});
+
+				// TODO: Send Warning Email
+			}
+		},
 	},
+
+	emailVerification: {
+		autoSignInAfterVerification: true,
+		sendOnSignUp: true,
+		async sendVerificationEmail({ user, url, token }, request) {
+			console.table({ user, url, token });
+
+			// TODO: Send Verification Email
+		},
+		expiresIn: 15 * 60 * 1000, // 15 minutes
+	},
+
+	baseURL: serverEnv.SERVER_URL,
 	plugins: [tanstackStartCookies()],
 });
