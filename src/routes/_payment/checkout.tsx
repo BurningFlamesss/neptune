75e43873ad78b55c6d0e38a30f34f9eaff1babee
mr67creative <mr67creative@gmail.com>
2026-09-20 23:36:11 +0545

    styled the checkout page with styles to make it look professional

--- src/routes/_payment/checkout.tsx
+++ src/routes/_payment/checkout.tsx
@@ -136,62 +136,89 @@ function RouteComponent() {
 	};
 
 	return (
-		<main>
-			<h1>Billing</h1>
-
-			<h1>{plan.name}</h1>
-			<p className="line-through">
-				{isCouponValid && formatPrice(plan.price, plan.currency)}
-			</p>
-			<p>{formatPrice(finalPrice, plan.currency)}</p>
-
-			<label htmlFor="code">Have a coupon?</label>
-			<input
-				type="text"
-				name="code"
-				placeholder="eg NEPTUNE20"
-				autoComplete="off"
-				spellCheck={false}
-				disabled={redeemMutation.isPending}
-				value={code}
-				onChange={(e) => setCode(e.target.value.toUpperCase())}
-			/>
-
-			<div>
-				{inspectQuery.isFetching && "Checking..."}
-
-				{!inspectQuery.isFetching &&
-					inspection &&
-					(inspection.valid ? (
-						<span>
-							Coupon applied!{" "}
-							{inspection.type === "PERCENTAGE_DISCOUNT"
-								? `${inspection.percentageDiscount}% off`
-								: `${formatPrice(inspection.fixedDiscount || 0, plan.currency)} off`}
+		<main className="max-w-5xl mx-auto flex flex-col justify-center">
+			<div className="grid grid-cols-2 gap-[15%] pt-18 pb-15">
+				<section className="bg-cyan/20 rounded-lg h-fit w-fit px-2 py-4 flex flex-col" key={plan.id}>
+					<h1 className="text-3xl">
+						{plan.name}
+					</h1>
+					<p className="text-lg flex flex-row gap-1">
+						<span className="line-through">
+							{isCouponValid && formatPrice(plan.price, plan.currency)}
 						</span>
-					) : (
-						<span>{inspection.reason || "Invalid coupon"}</span>
-					))}
-			</div>
+						<span>{formatPrice(finalPrice, plan.currency)}</span>
+					</p>
+					<ul className="flex flex-col items-start justify-center mt-4">
+						<li>Unlimited Collections</li>
+						<li>Unlimited knowledge items</li>
+						<li>Unlimited application connections</li>
+						<li>Intelligence Storage: <span className="text-ink-deep font-semibold">{plan.intelligenceStorage}</span></li>
+						<li>Sync: <span className="text-ink-deep font-semibold">{plan.sync}</span></li>
+						<li>Recall: <span className="text-ink-deep font-semibold">{plan.recall}</span></li>
+						<li>Versioning: <span className="text-ink-deep font-semibold">{plan.versioning}</span></li>
+						<li>Sharing: <span className="text-ink-deep font-semibold">{plan.sharing}</span></li>
+						{((plan.features as Array<string>) ?? [])?.map(
+							(feature, index) => (
+								<li key={`${index}-${feature}`}>{feature}</li>
+							),
+						)}
+					</ul>
+				</section>
 
-			<section>
-				{isCouponValid &&
-				(inspection.redeemptionType === "DIRECT_REDEEM" || finalPrice === 0) ? (
-					<button
-						type="button"
-						onClick={handleDirectRedeem}
+
+				<section>
+
+					<label htmlFor="code">Have a coupon?</label>
+					<input
+						type="text"
+						name="code"
+						placeholder="eg NEPTUNE20"
+						autoComplete="off"
+						spellCheck={false}
 						disabled={redeemMutation.isPending}
-					>
-						{redeemMutation.isPending
-							? "Redeeming..."
-							: "Redeem & Activate Plan"}
-					</button>
-				) : (
-					<button type="button" onClick={handleCheckout}>
-						Pay {formatPrice(finalPrice, plan.currency)}
-					</button>
-				)}
-			</section>
+						value={code}
+						onChange={(e) => setCode(e.target.value.toUpperCase())}
+					/>
+
+					<div className="mt-2">
+						{inspectQuery.isFetching && "Checking..."}
+
+						{!inspectQuery.isFetching &&
+							inspection &&
+							(inspection.valid ? (
+								<span>
+									Coupon applied!{" "}
+									{inspection.type === "PERCENTAGE_DISCOUNT"
+										? `${inspection.percentageDiscount}% off`
+										: `${formatPrice(inspection.fixedDiscount || 0, plan.currency)} off`}
+								</span>
+							) : (
+								<span>{inspection.reason || "Invalid coupon"}</span>
+							))}
+					</div>
+
+					<section className="mt-4">
+						{isCouponValid &&
+							(inspection.redeemptionType === "DIRECT_REDEEM" || finalPrice === 0) ? (
+							<button
+								className="app-button"
+								type="button"
+								onClick={handleDirectRedeem}
+								disabled={redeemMutation.isPending}
+							>
+								{redeemMutation.isPending
+									? "Redeeming..."
+									: "Redeem & Activate Plan"}
+							</button>
+						) : (
+							<button className="app-button" type="button" onClick={handleCheckout}>
+								Pay {formatPrice(finalPrice, plan.currency)}
+							</button>
+						)}
+					</section>
+				</section>
+
+			</div>
 		</main>
 	);
 }
