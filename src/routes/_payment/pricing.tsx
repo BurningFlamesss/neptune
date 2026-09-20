c23bdaf03758acd82e23560909c87ac8293b2a07
mr67creative <mr67creative@gmail.com>
2026-09-20 23:26:10 +0545

    styled the pricing page to show the plans more nicely

--- src/routes/_payment/pricing.tsx
+++ src/routes/_payment/pricing.tsx
@@ -16,35 +16,39 @@ function RouteComponent() {
 		return <div>No any packs available</div>;
 	}
 	return (
-		<main className="flex flex-row justify-between mx-8">
+		<main className="flex flex-row justify-between mx-4 mt-4">
 			{plans.map((plan) => {
 				return (
-					<section key={plan.id}>
-						<h1>
-							{plan.name}, {plan._count.payments} users purchased it
+					<section className="bg-cyan/20 rounded-lg h-130 px-2 py-4 flex flex-col" key={plan.id}>
+						<h1 className="text-3xl">
+							{plan.name}
 						</h1>
-						<p>{formatPrice(plan.price, plan.currency)}</p>
-						<ul>
+						<p className="text-lg">{formatPrice(plan.price, plan.currency)}</p>
+						<ul className="flex flex-col items-start justify-center mt-4">
 							<li>Unlimited Collections</li>
 							<li>Unlimited knowledge items</li>
 							<li>Unlimited application connections</li>
-							<li>Intelligence Storage: {plan.intelligenceStorage}</li>
-							<li>Sync: {plan.sync}</li>
-							<li>Recall: {plan.recall}</li>
-							<li>Versioning: {plan.versioning}</li>
-							<li>Sharing: {plan.sharing}</li>
+							<li>Intelligence Storage: <span className="text-ink-deep font-semibold">{plan.intelligenceStorage}</span></li>
+							<li>Sync: <span className="text-ink-deep font-semibold">{plan.sync}</span></li>
+							<li>Recall: <span className="text-ink-deep font-semibold">{plan.recall}</span></li>
+							<li>Versioning: <span className="text-ink-deep font-semibold">{plan.versioning}</span></li>
+							<li>Sharing: <span className="text-ink-deep font-semibold">{plan.sharing}</span></li>
 							{((plan.features as Array<string>) ?? [])?.map(
 								(feature, index) => (
 									<li key={`${index}-${feature}`}>{feature}</li>
 								),
 							)}
 						</ul>
-						<Link
+						<div className="flex flex-row gap-2 items-center justify-start mt-auto">
+							<Link
 							to="/checkout"
+							className="app-button"
 							search={{ plan: plan.name, billing: "monthly" }}
 						>
 							Choose this
-						</Link>
+						</Link> <p> {plan._count.payments} users purchased it</p>
+						</div>
+
 					</section>
 				);
 			})}
