679146a61d9a5db9b63e5b97025b11e20cb4a336
mr67creative <mr67creative@gmail.com>
2026-09-21 04:32:01 +0545

    made the basic UI for the recall section

--- src/routes/_public/recall.tsx
+++ src/routes/_public/recall.tsx
@@ -6,8 +6,13 @@ export const Route = createFileRoute('/_public/recall')({
 
 function RouteComponent() {
   return (
-    <main className='min-w-5xl mx-auto'>
-        <input type="text" name="prompt" id="prompt" />
+    <main className='max-w-5xl mx-auto flex flex-col justify-center'>
+        <section className='relative flex flex-col h-[80vh] overflow-y-auto'>
+            <span className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>Start your recall session</span>
+        </section>
+        <section className='flex flex-row items-center justify-center'>
+            <input type="text" name="prompt" id="prompt" className='text-lg!' /> <button className='app-button'>Recall</button>
+        </section>
     </main>
   )
 }
