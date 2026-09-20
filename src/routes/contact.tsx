f3adf5742fba3280ae82dcdaf312abc01fb2feef
mr67creative <mr67creative@gmail.com>
2026-09-20 08:45:20 +0545

    add the styles to the contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -8,37 +8,32 @@ export const Route = createFileRoute('/contact')({
 
 function RouteComponent() {
     return (
-        <main>
-            <div>
-                <span>A conversation starts somewhere</span>
-                <span>NepTune / Contact</span>
-            </div>
+        <main className="contact-page wrap max-w-5xl mx-auto flex flex-col justify-center">
+            <div className='contact-layout grid grid-cols-2 gap-[15%] pt-18 pb-15'>
+                <div className='relative'>
+                    <h1 className='text-7xl'>Let's <span className='text-cyan-dark'>talk.</span></h1>
 
-            <div>
-                <div>
-                    <h1>Let's<br /> talk.</h1>
-
-                    <div>
+                    <div className='contact-prompts mt-8 text-[16px] leading-[1.8]'>
                         <p>Have an idea?</p>
                         <p>Found something broken?</p>
                         <p>Build something with NepTune?</p>
                     </div>
 
-                    <p>
+                    <p className='contact-intro text-xs text-muted mt-5'>
                         We're interested in the things <br /> you're thinking about.
                     </p>
 
-                    <div>
+                    <div className='contact-ring'>
 
                     </div>
                 </div>
 
-                <div>
-                    <div>
-                        <span>
+                <div className='contact-form-column pt-1.5'>
+                    <div className='form-topline flex items-center justify-between mb-8'>
+                        <span className='mono text-[8px] text-[#7d9095]'>
                             Your words, a little space
                         </span>
-                        <ArrowUpRight size={18} strokeWidth={1} />
+                        <ArrowUpRight className='text-cyan-dark' size={18} strokeWidth={1} />
                     </div>
 
                     {/* TODO: Add Form */}
@@ -47,11 +42,11 @@ function RouteComponent() {
                 </div>
             </div>
 
-            <div>
-                <span>
+            <div className='contact-bottom flex justify-between items-center gap-7.5 pt-7.5 pb-9 border-t border-t-line'>
+                <span className='mono text-[8px] text-[#7d9095]'>
                     Good questions are a good beginning.
                 </span>
-                <Link to="/">First, a little about us <ArrowUpRight/></Link>
+                <Link className='text-button text-xs flex justify-center items-center' to="/">First, a little about us <ArrowUpRight size={18} /></Link>
             </div>
         </main>
     )
