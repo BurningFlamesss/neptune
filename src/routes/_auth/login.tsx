41f351a112985742589c87840b2066bc7e8eb0d6
mr67creative <mr67creative@gmail.com>
2026-09-20 08:58:02 +0545

    add the similar style of signup and contact to login and reset-password page

--- src/routes/_auth/login.tsx
+++ src/routes/_auth/login.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, Link } from "@tanstack/react-router";
 import type React from "react";
 import { z } from "zod";
 import { authClient } from "#/lib/auth-client.ts";
+import { ArrowUpRight } from "lucide-react";
 
 const loginSchema = z.object({
 	email: z.email(),
@@ -55,16 +56,49 @@ function RouteComponent() {
 	};
 
 	return (
-		<main>
-			<form onSubmit={login} action="#" method="post">
-				<label htmlFor="email">Email</label>{" "}
-				<input type="email" name="email" id="email" required /> <br />
-				<label htmlFor="password">Password</label>{" "}
-				<input type="password" name="password" id="password" required /> <br />
-				Forgot Password? <Link to="/reset-password">Reset Password</Link> <br />
-				<button type="submit">Login</button>
-			</form>
-			New to the App? <Link to="/signup">Sign Up</Link>
+		<main className="login-page wrap max-w-5xl mx-auto flex flex-col justify-center">
+			<div className='login-layout grid grid-cols-2 gap-[15%] pt-18 pb-15'>
+				<div className='relative'>
+					<h1 className='text-7xl'>Unlock your <span className='text-cyan-dark'>2nd Brain.</span></h1>
+
+					<div className='login-prompts mt-8 text-[16px] leading-[1.8]'>
+						<p className="">having,</p>
+						<p>Unlimited Collections</p>
+						<p>Unlimited Knowledges</p>
+						<p>Unlimited Application Connections</p>
+						<p>And more...</p>
+					</div>
+
+					<p className='login-intro text-xs text-muted mt-5'>
+						We're so excited to <br /> have you onboard.
+					</p>
+
+					<div className='login-ring'>
+
+					</div>
+				</div>
+
+				<div className='login-form-column pt-1.5'>
+					<div className='form-topline flex items-center justify-between mb-8'>
+						<span className='mono text-[8px] text-[#7d9095]'>
+							Your Intelligence, Merging Brilliant
+						</span>
+						<ArrowUpRight className='text-cyan-dark' size={18} strokeWidth={1} />
+					</div>
+
+					<form onSubmit={login} action="#" method="post">
+						<label htmlFor="email">Email</label>{" "}
+						<input type="email" name="email" id="email" required /> <br />
+						<label htmlFor="password">Password</label>{" "}
+						<input type="password" name="password" id="password" required /> <br />
+						Forgot Password? <Link to="/reset-password">Reset Password</Link> <br />
+						<button type="submit">Login</button>
+					</form>
+					New to the App? <Link to="/signup">Sign Up</Link>
+
+
+				</div>
+			</div>
 		</main>
 	);
 }
--- src/routes/_auth/reset-password.tsx
+++ src/routes/_auth/reset-password.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
 import { z } from "zod";
 import { authClient } from "#/lib/auth-client.ts";
+import { ArrowUpRight } from "lucide-react";
 
 const resetPasswordSearchParamSchema = z.object({
 	token: z.string().optional().default(""),
@@ -106,35 +107,68 @@ function RouteComponent() {
 	};
 
 	return (
-		<main>
-			{!token ? (
-				<form onSubmit={requestResetPassword} action="#" method="post">
-					<label htmlFor="email">Email</label>{" "}
-					<input type="email" name="email" id="email" required /> <br />
-					<button type="submit">Request Reset Password</button>
-				</form>
-			) : (
-				<form onSubmit={resetPassword} action="#" method="post">
-					<label htmlFor="newPassword">New Password</label>{" "}
-					<input
-						type="password"
-						name="newPassword"
-						id="newPassword"
-						required
-					/>{" "}
-					<br />
-					<label htmlFor="confirmPassword">Confirm Password</label>{" "}
-					<input
-						type="password"
-						name="confirmPassword"
-						id="confirmPassword"
-						required
-					/>{" "}
-					<br />
-					<button type="submit">Reset Password</button>
-				</form>
-			)}
-			New to the App? <Link to="/signup">Sign Up</Link>
+		<main className="reset-password-page wrap max-w-5xl mx-auto flex flex-col justify-center">
+			<div className='reset-password-layout grid grid-cols-2 gap-[15%] pt-18 pb-15'>
+				<div className='relative'>
+					<h1 className='text-7xl'>Unlock your <span className='text-cyan-dark'>2nd Brain.</span></h1>
+
+					<div className='reset-password-prompts mt-8 text-[16px] leading-[1.8]'>
+						<p className="">having,</p>
+						<p>Unlimited Collections</p>
+						<p>Unlimited Knowledges</p>
+						<p>Unlimited Application Connections</p>
+						<p>And more...</p>
+					</div>
+
+					<p className='reset-password-intro text-xs text-muted mt-5'>
+						We're so excited to <br /> have you onboard.
+					</p>
+
+					<div className='reset-password-ring'>
+
+					</div>
+				</div>
+
+				<div className='reset-password-form-column pt-1.5'>
+					<div className='form-topline flex items-center justify-between mb-8'>
+						<span className='mono text-[8px] text-[#7d9095]'>
+							Your Intelligence, Merging Brilliant
+						</span>
+						<ArrowUpRight className='text-cyan-dark' size={18} strokeWidth={1} />
+					</div>
+
+					{!token ? (
+						<form onSubmit={requestResetPassword} action="#" method="post">
+							<label htmlFor="email">Email</label>{" "}
+							<input type="email" name="email" id="email" required /> <br />
+							<button type="submit">Request Reset Password</button>
+						</form>
+					) : (
+						<form onSubmit={resetPassword} action="#" method="post">
+							<label htmlFor="newPassword">New Password</label>{" "}
+							<input
+								type="password"
+								name="newPassword"
+								id="newPassword"
+								required
+							/>{" "}
+							<br />
+							<label htmlFor="confirmPassword">Confirm Password</label>{" "}
+							<input
+								type="password"
+								name="confirmPassword"
+								id="confirmPassword"
+								required
+							/>{" "}
+							<br />
+							<button type="submit">Reset Password</button>
+						</form>
+					)}
+					New to the App? <Link to="/signup">Sign Up</Link>
+
+
+				</div>
+			</div>
 		</main>
 	);
 }
