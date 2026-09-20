93e5e2f7295f1219ee269674d2be12df47cbee77
mr67creative <mr67creative@gmail.com>
2026-09-21 03:31:17 +0545

    remove the unused block of code and add the Route Targetting in dashboard too

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,13 @@
 import { authClient } from '#/lib/auth-client'
 import { Link, useLoaderData } from '@tanstack/react-router'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from './ui/dropdown-menu'
-// import { userStore } from '#/store/user'
-// import { useQuery } from '@tanstack/react-query'
-// import { getUserPlan } from '#/functions/payment'
-// import { useEffect } from 'react'
 
 function Navbar() {
     const { data } = authClient.useSession()
     const isValidUser = data?.user.id
     const user = data?.user
 
-
     const { plan } = useLoaderData({ from: "__root__" })
-
-    // const plan = userStore((state) => state.plan)
-    // const update = userStore((state) => state.update)
-
-    // const { data: fetchedPlan } = useQuery({
-    //     queryKey: ["user-plan", user?.id],
-    //     queryFn: () => getUserPlan({
-    //         data: {
-    //             userId: user?.id
-    //         }
-    //     }),
-    //     enabled: !!user?.id,
-    //     staleTime: 5 * 60 * 1000
-    // })
-
-    // useEffect(() => {
-    //     if (fetchedPlan) {
-    //         update("plan", fetchedPlan)
-    //     }
-    // }, [fetchedPlan?.id, update])
 
     return (
         <header className='max-w-6xl mx-auto flex flex-row items-center justify-between'>
--- src/routes/_public/dashboard.tsx
+++ src/routes/_public/dashboard.tsx
@@ -1,16 +1,18 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useLoaderData } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_public/dashboard')({
-    
     component: RouteComponent,
 })
 
 function RouteComponent() {
     const context = Route.useRouteContext()
+    const { plan } = useLoaderData({ from: "__root__" })
+
 
     return (
         <main>
-            Hello, {context.session?.user.name}!!! You have plan
+            Hello, {context.session?.user.name}!!! You have {plan?.name} plan <br />
+            
         </main>
     )
 }
