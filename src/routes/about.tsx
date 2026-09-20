7411ff138af7be90b47db02e4159e80b0ec0a9e2
mr67creative <mr67creative@gmail.com>
2026-09-20 23:55:22 +0545

    add some content to the about page

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -5,5 +5,22 @@ export const Route = createFileRoute('/about')({
 })
 
 function RouteComponent() {
-  return <div>About</div>
+  return (
+    <main className='max-w-5xl mx-auto flex flex-col justify-center mt-4'>
+        <h1 className='text-3xl'>About <span className='text-cyan-dark'>NepTune</span> & its <span className='text-cyan-dark'>Ring</span></h1>
+
+        <section className='mt-8'>
+          <h2 className='text-lg'>## What is this?</h2>
+          <p>
+            We are building the definitive engine for automation in syncing, and reusable knowledge nodes. Our mission is to elminate the friction between thought and acting on it.
+          </p>
+        </section>
+        <section className='mt-4'>
+          <h2 className='text-lg'>## Recall</h2>
+          <p>
+            INformation is useless if you can't find it. We integrate model with chosen access to your knowledge so your knowledge can be retrieved from the source of your own intelligence, and get response visually.
+          </p>
+        </section>
+    </main>
+  )
 }
